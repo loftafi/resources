d600ad26a7ed2c5f3ce25d0b64d9cc8b0607036b
Jay <loftafi@icloud.com>
2025-08-25 19:46:23 +1000

    Dont hardcode ndk location

--- build.zig
+++ build.zig
@@ -88,15 +88,22 @@ pub fn add_imports(
         },
         .linux => {
             // When building for android, we need to use the android linux headers
-            const android_ndk = "/Users/macuser/Library/Android/sdk/ndk/27.0.12077973/";
-            lib.addSystemIncludePath(.{ .cwd_relative = b.pathJoin(&.{
-                android_ndk,
-                "toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/",
-            }) });
-            lib.addSystemIncludePath(.{ .cwd_relative = b.pathJoin(&.{
-                android_ndk,
-                "toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/aarch64-linux-android/",
-            }) });
+            if (FindNDK.find(b.allocator)) |android_ndk| {
+                const ndk_location = android_ndk.realpathAlloc(b.allocator, ".") catch {
+                    @panic("printing ndk path failed");
+                };
+                defer b.allocator.free(ndk_location);
+                lib.addSystemIncludePath(.{ .cwd_relative = b.pathJoin(&.{
+                    ndk_location,
+                    "toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/",
+                }) });
+                lib.addSystemIncludePath(.{ .cwd_relative = b.pathJoin(&.{
+                    ndk_location,
+                    "toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/aarch64-linux-android/",
+                }) });
+            } else {
+                @panic("android/linux build requires ndk. Set ANDROID_NDK_HOME");
+            }
         },
         else => {
             std.log.debug(
@@ -107,3 +114,138 @@ pub fn add_imports(
         },
     }
 }
+
+const FindNDK = struct {
+    const ndk_versions = [_][]const u8{
+        "29.0.13846066", // Pre-release
+        "28.2.13676358", // Stable
+        "27.3.13750724", // LTS
+        "27.0.12077973",
+        "25.1.8937393",
+        "23.2.8568313",
+        "23.1.7779620",
+        "21.0.6113669",
+        "20.1.5948944",
+    };
+
+    pub fn find(gpa: std.mem.Allocator) ?std.fs.Dir {
+        const android_ndk_home = find_android_ndk_home(gpa) catch |e| {
+            std.log.err("error while searching for ndk: {any}", .{e});
+            return null;
+        };
+        if (android_ndk_home != null) return android_ndk_home.?;
+
+        const android_sdk_root = find_android_sdk_root(gpa) catch |e| {
+            std.log.err("error while searching for sdk: {any}", .{e});
+            return null;
+        };
+        if (android_sdk_root != null) {
+            if (android_sdk_root.?.openDir("ndk", .{})) |dir| {
+                std.log.debug("searching inside ANDROID_SDK_ROOT/ndk", .{});
+                const found = search_ndk_folder(gpa, dir);
+                if (found != null) return found.?;
+            } else |_| {
+                std.log.debug("no ndk in ANDROID_SDK_ROOT", .{});
+            }
+        }
+
+        const home = find_user_home(gpa) catch |e| {
+            std.log.err("error while searching for ndk: {any}", .{e});
+            return null;
+        };
+        if (home == null) {
+            std.log.err("ndk not found. No HOME or USERPROFILE set.", .{});
+            return null;
+        }
+        const ndk_base = home.?.openDir("Library/Android/sdk/ndk/", .{}) catch |e| {
+            std.log.err("ndk not found. Error {any} reading HOME/Library/Android/sdk/ndk/", .{e});
+            return null;
+        };
+        return search_ndk_folder(gpa, ndk_base);
+    }
+
+    pub fn search_ndk_folder(_: std.mem.Allocator, ndk_base: std.fs.Dir) ?std.fs.Dir {
+        for (ndk_versions) |version| {
+            const folder = ndk_base.openDir(version, .{}) catch {
+                continue;
+            };
+            std.log.debug("ndk version found: {any}", .{folder});
+            return folder;
+        }
+        return null;
+    }
+
+    /// If ANDROID_NDK_HOME is set, just use that
+    pub fn find_android_ndk_home(gpa: std.mem.Allocator) !?std.fs.Dir {
+        var env_map = try std.process.getEnvMap(gpa);
+        defer env_map.deinit();
+        var iter = env_map.iterator();
+        var home: ?[]const u8 = null;
+        while (iter.next()) |entry| {
+            if (std.ascii.eqlIgnoreCase("ANDROID_NDK_HOME", entry.key_ptr.*)) {
+                home = entry.value_ptr.*;
+                std.debug.print("{s}={s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+                break;
+            }
+        }
+        if (home == null) {
+            std.log.info("ANDROID_NDK_HOME not set.", .{});
+            return null;
+        }
+        const d = std.fs.openDirAbsolute(home.?, .{}) catch {
+            std.log.warn("Failed to read ANDROID_NDK_HOME directory {any}", .{home.?});
+            return null;
+        };
+        return d;
+    }
+
+    /// If ANDROID_SDK_ROOT is set, just use that
+    pub fn find_android_sdk_root(gpa: std.mem.Allocator) !?std.fs.Dir {
+        var env_map = try std.process.getEnvMap(gpa);
+        defer env_map.deinit();
+        var iter = env_map.iterator();
+        var home: ?[]const u8 = null;
+        while (iter.next()) |entry| {
+            if (std.ascii.eqlIgnoreCase("ANDROID_SDK_ROOT", entry.key_ptr.*)) {
+                home = entry.value_ptr.*;
+                std.debug.print("{s}={s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+                break;
+            }
+        }
+        if (home == null) {
+            std.log.info("ANDROID_SDK_ROOT not set.", .{});
+            return null;
+        }
+        const d = std.fs.openDirAbsolute(home.?, .{}) catch {
+            std.log.warn("Failed to read ANDROID_SDK_ROOT directory {any}", .{home.?});
+            return null;
+        };
+        return d;
+    }
+
+    /// Sometimes, the NDK is in the users home folder
+    pub fn find_user_home(gpa: std.mem.Allocator) !?std.fs.Dir {
+        var env_map = try std.process.getEnvMap(gpa);
+        defer env_map.deinit();
+        var iter = env_map.iterator();
+        var home: ?[]const u8 = null;
+        while (iter.next()) |entry| {
+            if (std.ascii.eqlIgnoreCase("HOME", entry.key_ptr.*)) {
+                home = entry.value_ptr.*;
+                std.debug.print("{s}={s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+            }
+            if (std.ascii.eqlIgnoreCase("UserProfile", entry.key_ptr.*)) {
+                home = entry.value_ptr.*;
+                std.debug.print("{s}={s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+            }
+        }
+        if (home != null) {
+            const d = std.fs.openDirAbsolute(home.?, .{}) catch {
+                std.log.warn("Failed to read directory {any}", .{home.?});
+                return null;
+            };
+            return d;
+        }
+        return null;
+    }
+};
