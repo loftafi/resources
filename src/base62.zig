a3b92fe41430321b5cd08f38bba5d83c262987c4
Jay <loftafi@icloud.com>
2025-12-12 17:44:49 +1000

    Add encode_writer()

--- src/base62.zig
+++ src/base62.zig
@@ -40,6 +40,30 @@ pub fn encode(comptime I: type, uid: I, result: *[40:0]u8) []const u8 {
     return result[0..i];
 }
 
+pub fn encode_writer(comptime I: type, uid: I) type {
+    return struct {
+        pub fn format(out: *std.Io.Writer) error{WriteFailed}!void {
+            var value: I = uid;
+
+            if (uid == 0) {
+                try out.writeByte('A');
+                return;
+            }
+            while (value > 0) {
+                const r = @rem(value, 62);
+                value = @divFloor(value, 62);
+                if (r < 26) {
+                    try out.writeByte('A' + @as(u8, @intCast(r)));
+                } else if (r < 52) {
+                    try out.writeByte('a' + (@as(u8, @intCast(r)) - 26));
+                } else {
+                    try out.writeByte('0' + (@as(u8, @intCast(r)) - 52));
+                }
+            }
+        }
+    };
+}
+
 /// Convert a base62 encoded string back to an integer.
 pub fn decode(comptime I: type, text: []const u8) error{
     InvalidBase62,
@@ -87,6 +111,26 @@ test "encode" {
     try seq("9", encode(u64, 61, &result));
     try seq("AB", encode(u64, 62, &result));
     try seq("AAB", encode(u64, 62 * 62, &result));
+}
+
+test "encode_stream" {
+    const gpa = std.testing.allocator;
+
+    const out = try std.fmt.allocPrint(gpa, "{f}", .{encode_writer(u8, 111)});
+    defer gpa.free(out);
+    try seq("xB", out);
+
+    const out2 = try std.fmt.allocPrint(gpa, "{f}", .{encode_writer(u8, 3)});
+    defer gpa.free(out2);
+    try seq("D", out2);
+
+    const out3 = try std.fmt.allocPrint(gpa, "{f}", .{encode_writer(usize, 62 * 62)});
+    defer gpa.free(out3);
+    try seq("AAB", out3);
+
+    const out4 = try std.fmt.allocPrint(gpa, "{f}", .{encode_writer(usize, 62 * 62 + 1)});
+    defer gpa.free(out4);
+    try seq("BAB", out4);
 }
 
 test "decode" {
